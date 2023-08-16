d54975d2f13f00290e905e2989a23a141eb0e20a
Paula Aschendorf <paula.aschendorf@gmail.com>
2023-08-16 11:25:55 +0200

    adds basic html structure, get and post function

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Todo App with Todo API
 
-- [ ] Create a folder (and repository) for a Todo App that will use the Todo API
-- [ ] Start from scratch and DO NOT copy any code
+- [x] Create a folder (and repository) for a Todo App that will use the Todo API
+- [x] Start from scratch and DO NOT copy any code
 - [ ] Use the Todo API with your new Todo App
-  - [ ] Load all todos from the Todo API
-  - [ ] Create todos in the Todo API
+  - [x] Load all todos from the Todo API
+  - [x] Create todos in the Todo API
   - [ ] Update todos in the Todo API whenever the done state is changed
   - [ ] Add a `Delete Done Todos` button that will delete all done todos from the Todo API
 - [ ] Keep your local state, backend state and the UI in sync
---
+++ script.js
@@ -0,0 +1,39 @@
+const inputToDo = document.querySelector("#input-to-do");
+const btnToDo = document.querySelector("#add-to-do-btn");
+const ulToDo = document.querySelector("#to-do-list");
+const urlToDoAPI = "http://localhost:4730/todos";
+
+let todos = [];
+function getRequestAPI() {
+  fetch(urlToDoAPI)
+    .then((request) => request.json())
+    .then((toDosFromAPI) => {
+      todos = toDosFromAPI;
+      renderState();
+    });
+}
+getRequestAPI();
+
+function renderState() {
+  todos.forEach((todo) => {
+    const newLi = document.createElement("li");
+    const toDoDescription = todo.description;
+    newLi.innerText = toDoDescription;
+    ulToDo.appendChild(newLi);
+  });
+}
+
+btnToDo.addEventListener("click", () => {
+  const newToDoDescription = inputToDo.value;
+  const newTodo = {
+    description: newToDoDescription,
+    done: false,
+  };
+  fetch(urlToDoAPI, {
+    method: "POST",
+    headers: { "content-Type": "application/json" },
+    body: JSON.stringify(newTodo),
+  })
+    .then((response) => response.json())
+    .then((backendState) => {});
+});
